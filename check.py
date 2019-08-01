f7826173ae3a7fcaeb231de10e494f92c96fdde7
michaal94 <michaal94@gmail.com>
2019-08-01 10:31:44 +0100

    initial commit

---
+++ check.py
@@ -0,0 +1,28 @@
+from conceptNet import ConceptNet
+import time
+
+# conceptnet = ConceptNet('assertions.csv', language='english', save_language=True)
+conceptnet = ConceptNet('assertions_english.pkl')
+start_time = time.time()
+# a = conceptnet.get_query(start=['apple'])
+dog = conceptnet.get_query(start=['dog'])
+# raw_data = a.get_raw_dataframe()
+end_time = time.time()
+# print(end_time - start_time)
+start_time = time.time()
+# j = raw_data.JSON
+# print(raw_data.to_string())
+# print(j)
+dog.process_data()
+print(dog.processed_df.to_string())
+dog_cat = dog.get_query(end=['cat'])
+dog_cat.process_data()
+print(dog_cat.processed_df.to_string())
+
+# print(a.processed_df.JSON.to_string())
+# print(a.processed_df.JSON[0]['surfaceEnd'])
+
+# print(a.df.start)
+# b = conceptnet.get_query(start=['horse'], relation=['RelatedTo'])
+# print(b.df.start)
+
---
+++ conceptNet.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import os
+import numpy as np
+
+
+lang_dict = {
+    'english': 'en'
+}
+
+
+class ConceptNet():
+    def __init__(self, data_path, language=None, save_language=False):
+        base, extention = os.path.splitext(data_path)
+        if extention == '.csv':
+            df = pd.read_csv(data_path, sep='\t', header=None,
+                             names=['URI', 'relation', 'start', 'end', 'JSON'])
+        elif extention == '.pkl':
+            df = pd.read_pickle(data_path)
+
+        if language is not None:
+            lang_abbr = '/' + get_language_abbr(language) + '/'
+            index = df[~df.start.str.contains(lang_abbr)].index
+            df.drop(index, inplace=True)
+            index = df[~df.end.str.contains(lang_abbr)].index
+            df.drop(index, inplace=True)
+            if save_language:
+                df.to_pickle(base + '_' + language + '.pkl')
+
+        self.df = df
+
+    def get_edges_from_start(self, start_tokens, dataframe=None):
+        start_tokens = self.process_tokens(start_tokens)
+        if dataframe is None:
+            edges = self.df[self.df.start.str.contains('|'.join(start_tokens))]
+        else:
+            edges = dataframe[dataframe.start.str.contains('|'.join(start_tokens))]
+        return edges
+
+    def get_edges_to_end(self, end_tokens, dataframe=None):
+        end_tokens = self.process_tokens(end_tokens)
+        if dataframe is None:
+            edges = self.df[self.df.end.str.contains('|'.join(end_tokens))]
+        else:
+            edges = dataframe[dataframe.end.str.contains('|'.join(end_tokens))]
+        return edges
+
+    def get_edges_by_relation(self, relation_tokens, dataframe=None):
+        relation_tokens = self.process_tokens(relation_tokens, relation=True)
+        if dataframe is None:
+            edges = self.df[self.df.relation.str.contains('|'.join(relation_tokens))]
+        else:
+            edges = dataframe[dataframe.relation.str.contains('|'.join(relation_tokens))]
+        return edges
+
+    def get_query(self, start=None, end=None, relation=None):
+        edges = self.df
+        if start is not None:
+            edges = self.get_edges_from_start(start, dataframe=edges)
+        if end is not None:
+            edges = self.get_edges_to_end(end, dataframe=edges)
+        if relation is not None:
+            edges = self.get_edges_by_relation(relation, dataframe=edges)
+        edges = edges.copy()
+        edges.reset_index(drop=True, inplace=True)
+        return EdgeFrame(edges)
+
+    def process_tokens(self, token_list, relation=False):
+        processed_list = []
+        for token in token_list:
+            new_token = token
+            # lower case as the concept net is
+            if not relation:
+                new_token = token.lower().replace(' ', '_')
+            # put regex such that word starts with / (like /c/en/word)
+            # and ends up with / or nothing - in order to match exact words
+            # new_token = '(?<!wp|wn)\\/' + new_token + '[\\/]|(?<!wp|wn)\\/' + new_token + '$'
+            new_token = '^\\/[^\\/]*\\/[^\\/]*\\/' + new_token + '\\/|^\\/[^\\/]*\\/[^\\/]*\\/' + new_token + '$'
+            # new_token = '\\/' + new_token
+            processed_list.append(new_token)
+        return processed_list
+
+
+class EdgeFrame(ConceptNet):
+    def __init__(self, dataframe):
+        self.df = dataframe
+
+    def get_raw_dataframe(self):
+        return self.df
+
+    def process_data(self):
+        self.processed_df = self.df.copy()
+        # print(self.processed_df.columns.values)
+        # print(self.processed_df)
+        self.processed_df = self.processed_df.reindex(columns=(list(self.processed_df.columns.values) + ['startPoS', 'endPoS', 'startHypernym', 'endHypernym', 'startSurface', 'endSurface', 'surfaceText', 'weight']))
+        self.processed_df[['start', 'startPoS', 'startHypernym']] = self.processed_df[['start', 'startPoS', 'startHypernym']].apply(process_node_tokens, axis=1)
+        self.processed_df[['end', 'endPoS', 'endHypernym']] = self.processed_df[['end', 'endPoS', 'endHypernym']].apply(process_node_tokens, axis=1)
+        self.processed_df[['startSurface', 'endSurface', 'surfaceText', 'weight']] = self.processed_df[['JSON']].apply(process_JSON, axis=1)
+        self.processed_df['relation'] = self.processed_df['relation'].map(process_relation)
+        self.processed_df.drop(columns=['URI', 'JSON'], inplace=True)
+        # process_node_tokens(self.processed_df.start)
+        # print(list(self.processed_df.columns.values) + ['startPoS', 'endPoS', 'hyperymStart', 'hypernymEnd'])
+
+
+def process_node_tokens(cols):
+    split = cols[0].strip('/').split('/')[2:]
+    name, pos, hypernym = np.nan, np.nan, np.nan
+    if len(split) > 2:
+        if split[2] in ['wp', 'wn']:
+            hypernym = split[3]
+            split = split[:-2]
+    if len(split) > 1:
+        pos = split[1]
+    name = split[0]
+    return pd.Series([name, pos, hypernym])
+
+
+def process_relation(relation):
+    split = relation.strip('/').split('/')
+    if len(split) > 2:
+        return split[2]
+    else:
+        return split[1]
+
+
+def process_JSON(json_col):
+    json_col = eval(json_col.values[0])
+    startSurface, endSurface, surfaceText, weight = np.nan, np.nan, np.nan, np.nan
+    if 'surfaceStart' in json_col:
+        startSurface = json_col['surfaceStart']
+    if 'surfaceEnd' in json_col:
+        endSurface = json_col['surfaceEnd']
+    if 'surfaceText' in json_col:
+        surfaceText = json_col['surfaceText']
+    if 'weight' in json_col:
+        weight = json_col['weight']
+
+    series = [startSurface, endSurface, surfaceText, weight]
+
+    return pd.Series(series)
+
+
+def get_language_abbr(language):
+    if language in lang_dict:
+        return lang_dict[language]
+    else:
+        raise NotImplementedError('Language not implemented or not present')
